a636a01121c333d19d1a6777ce575c491872c0b3
Jaroly456 <jaroly.vidaldiaz25@compscihigh.org>
2025-01-07 22:24:14 +0000

    PA complete

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/UTDHf9Ow)
 [![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-2e0aaae1b6195c2367325f4f02e2d04e9abb55f0b24a779b69b11b9e10269abc.svg)](https://classroom.github.com/online_ide?assignment_repo_id=17636462&assignment_repo_type=AssignmentRepo)
 # node-template
+
+1. Why did you pick this API topic?
+    I picked this API topic for people who want to know how many teams are in the Japan SV.League. While also learning about the volleyball positions
+
+2. Describe your API and the available data endpoints
+    My API gives users information on the Japan volleyball teams for men, and all positions in volleyball. The available endpoints are /docs, /api/volleyball/teams, /api/volleyball/teams/:id, /api/volleyball/positions, and /api/volleyball/positions:idNum. /doc tells you what all the available endpoints are for the user to search. /api/volleyball/teams shows you all teams in the volleyball object, while /api/volleyball/teams/:id shows you the teams individually. /api/volleyball/positions shows all volleyball positions, while /api/volleyball/positions/:idNum shows the positions individually.
+
+3. How was your experience building your own API server after using other APIs as a client in Unit 2?
+    It was an interesting expierence. Building my own API service at this level felt a lot easier than I believe it should be. Which has me a little worried about future coding projects we would have to make
--- index.js
+++ index.js
@@ -1,1 +1,115 @@
 // Good luck!
+
+const express = require('express');
+const { networkInterfaces } = require('os');
+const app = express()
+
+let volleyball = {
+    position: [
+        {
+          name: "Setter",
+          description: "The playmaker responsible for setting the ball for attackers."
+        },
+        {
+          name: "Outside Hitter",
+          description: "The primary attacker who scores points from the front and back row."
+        },
+        {
+          name: "Middle Blocker",
+          description: "Specializes in blocking and quick middle attacks near the net."
+        },
+        {
+          name: "Opposite Hitter",
+          description: "A versatile attacker positioned opposite the setter, aiding in both offense and defense."
+        },
+        {
+        name: "Libero",
+          description: "A defensive specialist who excels in passing and digging."
+        },
+        {
+          name: "Defensive Specialist",
+          description: "Focused on defense and controlling the ball during crucial plays."
+        }
+      ],
+    japanTeams: [
+        { name: "Voreas Hokkaido", 
+          playerCount: "16", 
+          headCoach: "Klein Edo" 
+        },
+        { name: "Tokyo Great Bears", 
+          playerCount: "17", 
+          headCoach: "Vourinen Kasper" 
+        },
+        { name: "VC Nagano Tridents", 
+          playerCount: "20", 
+          headCoach: "Shinji Kawamura" 
+        },
+        { name: "Toray Arrows Shizuoka", 
+          playerCount: "18", 
+          headCoach: "Yuta Abe" 
+        },
+        { name: "Jtekt Stings Aichi", 
+          playerCount: "17", 
+          headCoach: "Michal Gogol" 
+        },
+        { name: "Wolfdogs Nagoya", 
+          playerCount: "16", 
+          headCoach: "Baldovin Valrio" 
+        },
+        { name: "Osaka Blueteon", 
+          playerCount: "15", 
+          headCoach: "Tillie Laurent" 
+        },
+        { name: "Suntory Sunbirds Osaka", 
+          playerCount: "19", 
+          headCoach: "Lecat Olivier" 
+        },
+        { name: "Nippon Steel Sakai Blazers", 
+          playerCount: "16", 
+          headCoach: "Kitajima Takeshi" 
+        },
+        { name: "Hiroshima Thunders", 
+          playerCount: "21", 
+          headCoach: "Carlos Javier Weber" 
+        }
+    ]
+}
+
+app.use((req, res, next) => {
+    console.log(req.method + "/" + req.url)
+    next()
+})
+
+app.get("/", (req, res, next) => {
+    res.status(200).send("<h2>Welcome to the Japan SV.Leauge volleyball API</h2>")
+})
+
+app.get("/docs", (req, res, next) => {
+  res.status(200).send("All available routes are /api/volleyball/positions, /api/volleyball/positions/:idNum, /api/volleyball/teams, /api/volleyball/teams/id")
+})
+
+app.get("/api/volleyball/teams", (req, res, next) => {
+  res.status(200).json(volleyball.japanTeams)
+})
+
+app.get("/api/volleyball/teams/:id", (req, res, next) => {
+  const id = req.params.id
+  res.status(200).json(volleyball.japanTeams[id])
+})
+
+app.get("/api/volleyball/positions", (req, res, next) => {
+  res.status(200).json(volleyball.position)
+})
+
+app.get("/api/volleyball/positions/:idNum", (req, res, next) => {
+  const idNum = req.params.idNum
+  res.status(200).json(volleyball.position[idNum])
+})
+
+app.use((req, res, next) => {
+  res.status(404).send("404 NOT FOUND")
+})
+
+app.listen(3000, () => {
+    console.log("Server running")
+})
